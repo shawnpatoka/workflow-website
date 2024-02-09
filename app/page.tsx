470975a9e341ed53c869f9f3c995733a6023eb6f
Shawn Patoka <dev-services@shawnpatoka.com>
2024-02-09 16:23:19 -0600

    replaced h1 with temp logo

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,8 @@ export default function Home() {
   return (
     <main className="flex h-dvh items-center justify-center p-12">
       <div className="w-full sm:w-1/2 text-center">
-        <h1 className="text-5xl lg:text-8xl md:text-7xl sm:text-6xl text-white mb-3">WorkFlow<small className="block text-base sm:text-xl">Work Order Management</small></h1>
-        <hr className="w-1/3 mx-auto mb-2" />
-        <p className="text-white">Streamline work orders for service teams. Create, assign tasks, and integrate seamlessly with Slack and Google Calendars for efficient scheduling.
+        <Image src="/wf_logo.svg" width={450} height={150} alt="WorkFlow" className="mx-auto mb-6 drop-shadow-md" />
+        <p className="text-white drop-shadow-md">Streamline work orders for service teams. Create, assign tasks, and integrate seamlessly with Slack and Google Calendars for efficient scheduling.
         </p>
       </div>
     </main>
