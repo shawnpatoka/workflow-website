91c65fb7504f17268b5e30db0ee11baad0e24880
Shawn Patoka <dev-services@shawnpatoka.com>
2024-02-09 15:17:39 -0600

    made text responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center p-12">
       <div className="w-full sm:w-1/2 text-center">
-        <h1 className="text-8xl mb-3 text-white">WorkFlow<small className="block text-xl">Work Order Management</small></h1>
+        <h1 className="text-4xl lg:text-8xl md:text-7xl sm:text-6xl text-white mb-3">WorkFlow<small className="block text-base sm:text-xl">Work Order Management</small></h1>
         <hr className="w-1/3 mx-auto mb-2" />
         <p className="text-white">Streamline work orders for service teams. Create, assign tasks, and integrate seamlessly with Slack and Google Calendars for efficient scheduling.
         </p>
