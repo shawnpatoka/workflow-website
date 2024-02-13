f62b38f515164d1afb353780a37197b13fafb021
Shawn Patoka <dev-services@shawnpatoka.com>
2024-02-13 11:37:42 -0600

    switched to final logo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex h-dvh items-center justify-center p-12">
       <div className="w-full sm:w-1/2 text-center">
-        <Image src="/workflow_logo_darkmode.svg" width={450} height={150} alt="WorkFlow" className="mx-auto mb-6 drop-shadow-md" />
+        <Image src="/workflow_logo_full_darkmode_transparent.svg" width={450} height={150} alt="WorkFlow" className="mx-auto mb-6 drop-shadow-md" />
         <p className="text-white drop-shadow-md">Streamline work orders for service teams. Create, assign tasks, and integrate seamlessly with Slack and Google Calendars for efficient scheduling.
         </p>
       </div>
